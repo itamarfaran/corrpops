efc40df5b794f8e6263ffde4eb9a5e0cc4af8aa3
itamarf <itamar.faran@gmail.com>
2021-02-26 17:08:23 +0200

    omit vectorized data

--- R/data.R
+++ R/data.R
@@ -1,17 +1,3 @@
-# Correlation Matrices from the Research of Michael Peer et al on the Functional Connectivity of TGA Patients.
-#
-# A dataset containing two matrices of vectorized correlation matrices, with original dimension of 86x86,
-# of 23 control subjects and 14 TGA-diagnosed patients.
-#
-# @format A list of 2 matrices with 3,655 columns:
-# \describe{
-#   \item{control}{control subjects with n (rows) = 23}
-#   \item{diagnosed}{diagnosed subjects with n (rows) = 14}
-# }
-# @references Michael Peer et al. Reversible functional connectivity disturbances during transient global amnesia. Annals of neurology, 75(5):634–643, 2014.
-# "tga_matrices"
-
-
 #' Correlation Matrices from the Research of Michael Peer et al on the Functional Connectivity of TGA Patients.
 #'
 #' A dataset containing 86x86 correlation matrices of 23 control subjects and 14 TGA-diagnosed patients.
